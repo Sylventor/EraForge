75afa1aa8138ad815154dd1078575e776980ad8f
Sylventor <133502444+Sylventor@users.noreply.github.com>
2026-06-06 18:20:08 +0200

    Сделан клас Civilization

--- Civilization.cpp
+++ Civilization.cpp
@@ -3,3 +3,91 @@
 //
 
 #include "Civilization.h"
+
+// TODO: Сделать конструктор
+
+int const Civilization::getGold() {
+    return this->gold;
+}
+int const Civilization::getScience() {
+    return this->science;
+}
+std::map<Resource, int> const Civilization::getResources() {
+    return this->resources;
+}
+std::vector<City*> const Civilization::getCities() {
+    return this->cities;
+}
+std::vector<Unit*> const Civilization::getUnits() {
+    return this->units;
+}
+std::vector<Building> const Civilization::getResearchedBuildings() {
+    return this->researchedBuildings;
+}
+std::vector<Unit> const Civilization::getResearchedUnits() {
+    return this->researchedUnits;
+}
+
+bool Civilization::researchBuilding(Building building, int cost) {
+    if (this->science < cost) {
+        return false;
+    }
+    this->researchedBuildings.push_back(building);
+    this->science -= cost;
+    return true;
+}
+bool Civilization::researchUnit(Unit unit, int cost) {
+    if (this->science < cost) {
+        return false;
+    }
+    this->researchedUnits.push_back(unit);
+    this->science -= cost;
+    return true;
+}
+void Civilization::addGold(int amount) {
+    this->gold += amount;
+}
+void Civilization::addScience(int amount) {
+    this->science += amount;
+}
+void Civilization::addCity(City* city) {
+    this->cities.push_back(city);
+}
+void Civilization::addResource(Resource resource, int amount) {
+    this->resources[resource] += amount;
+}
+void Civilization::addUnit(Unit* unit) {
+    this->units.push_back(unit);
+}
+
+void Civilization::removeGold(int amount) {
+    if (this->gold > amount) {
+        this->gold -= amount;
+    } else {
+        this->gold = 0;
+    }
+}
+void Civilization::removeScience(int amount) {
+    if (this->science > amount) {
+        this->science -= amount;
+    } else {
+        this->science = 0;
+    }
+}
+void Civilization::removeResource(Resource resource, int amount) {
+    if (this->resources[resource] > amount) {
+        this->resources[resource] -= amount;
+    } else {
+        this->resources[resource] = 0;
+    }
+}
+void Civilization::removeCity(int index) {
+    delete cities[index];
+    cities.erase(cities.begin() + index);
+}
+void Civilization::removeUnit(int index) {
+    delete units[index];
+    units.erase(units.begin() + index);
+}
+
+void Civilization::update() {} // TODO: Доделать функцию update в Civilization
--- Civilization.h
+++ Civilization.h
@@ -4,9 +4,51 @@
 
 #ifndef UNTITLED_CIVILIZATION_H
 #define UNTITLED_CIVILIZATION_H
+#include <map>
+#include <vector>
 
+#include "Globals.h"
+
+class City{};
+class Unit{};
+class Building{};
 
 class Civilization {
+private:
+    int gold;
+    int science;
+    std::map<Resource, int> resources;
+    std::vector<City*> cities;
+    std::vector<Unit*> units;
+    std::vector<Building> researchedBuildings;
+    std::vector<Unit> researchedUnits;
+public:
+    Civilization();
+
+    int const getGold();
+    int const getScience();
+    std::map<Resource, int> const getResources();
+    std::vector<City*> const getCities();
+    std::vector<Unit*> const getUnits();
+    std::vector<Building> const getResearchedBuildings();
+    std::vector<Unit> const getResearchedUnits();
+
+    bool researchBuilding(Building building, int cost);
+    bool researchUnit(Unit unit, int cost);
+
+    void addGold(int amount);
+    void addScience(int amount);
+    void addResource(Resource resource, int amount);
+    void addCity(City* city);
+    void addUnit(Unit* unit);
+
+    void removeGold(int amount);
+    void removeScience(int amount);
+    void removeResource(Resource resource, int amount);
+    void removeCity(int index);
+    void removeUnit(int index);
+
+    void update();
 };
 
 
