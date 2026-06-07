b4bba4195083e03e25ed265bce39eea814dce613
Sylventor <133502444+Sylventor@users.noreply.github.com>
2026-06-07 12:02:43 +0200

    Сделан клас Building

--- Building.cpp
+++ Building.cpp
@@ -3,3 +3,52 @@
 //
 
 #include "Building.h"
+
+
+Building::Building(std::string name, char icon, BuildingType type, int foodBonus, int goldBonus, int populationBonus,
+                   int scienceBonus, int resourceBonus, int cost, Resource requiredResource,
+                   std::vector<TerrainType> requiredTerrain) {
+    this->name = name;
+    this->icon = icon;
+    this->type = type;
+    this->foodBonus = foodBonus;
+    this->goldBonus = goldBonus;
+    this->populationBonus = populationBonus;
+    this->resourceBonus = resourceBonus;
+    this->cost = cost;
+    this->requiredResource = requiredResource;
+    this->requiredTerrain = requiredTerrain;
+}
+
+std::string const Building::getName() {
+    return name;
+}
+char const Building::getIcon() {
+    return icon;
+}
+BuildingType const Building::getType() {
+    return type;
+}
+int const Building::getFoodBonus() {
+    return foodBonus;
+}
+int const Building::getGoldBonus() {
+    return goldBonus;
+}
+int const Building::getPopulationBonus() {
+    return populationBonus;
+}
+int const Building::getResourceBonus() {
+    return resourceBonus;
+}
+Resource const Building::getRequiredResource() {
+    return requiredResource;
+}
+std::vector<TerrainType> const Building::getRequiredTerrain() {
+    return requiredTerrain;
+}
+
+
+void Building::upgrade() {
+    // TODO: Доделать метод upgrade
+}
--- Building.h
+++ Building.h
@@ -20,10 +20,24 @@ private:
     int populationBonus;
     int scienceBonus;
     int resourceBonus;
+    int cost;
     Resource requiredResource;
     std::vector<TerrainType> requiredTerrain;
 public:
-    Building(std::string name, char icon, BuildingType type, int foodBonus, int goldBonus, int populationBonus, int scienceBonus, int resourceBonus, Resource requiredResource, std::vector<TerrainType> requiredTerrain);
+    Building(std::string name, char icon, BuildingType type, int foodBonus, int goldBonus, int populationBonus, int scienceBonus, int resourceBonus, int cost, Resource requiredResource, std::vector<TerrainType> requiredTerrain);
+
+    std::string const getName();
+    char const getIcon();
+    BuildingType const getType();
+    int const getFoodBonus();
+    int const getGoldBonus();
+    int const getPopulationBonus();
+    int const getScienceBonus();
+    int const getResourceBonus();
+    int const getCost();
+    Resource const getRequiredResource();
+    std::vector<TerrainType> const getRequiredTerrain();
+
     void upgrade();
 };
 
